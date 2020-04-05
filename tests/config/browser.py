5599711d2e13009f5e37aa467ae180259fc561c6
olakowalczyk <60587401+olakowalczyk@users.noreply.github.com>
2020-04-05 18:42:08 +0200

    format

--- tests/config/browser.py
+++ tests/config/browser.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 import chromedriver_autoinstaller
 chromedriver_autoinstaller.install()
 
+
 @pytest.fixture
 def browser():
-  driver = webdriver.Chrome()
-  driver.implicitly_wait(10)
-  yield driver
-  driver.quit()
-
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
